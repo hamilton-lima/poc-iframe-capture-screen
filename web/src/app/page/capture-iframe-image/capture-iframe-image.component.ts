a44588de996ce80666592e15fc699da5c24851bd
Hamilton Lima <hamilton@icouch.me>
2023-01-24 15:31:40 -0500

    Capture from other domain gets empty image, while capturing from current domain gets the image

--- web/src/app/page/capture-iframe-image/capture-iframe-image.component.ts
+++ web/src/app/page/capture-iframe-image/capture-iframe-image.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NgxCaptureService } from 'ngx-capture';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-capture-iframe-image',
   templateUrl: './capture-iframe-image.component.html',
-  styleUrls: ['./capture-iframe-image.component.scss']
+  styleUrls: ['./capture-iframe-image.component.scss'],
 })
 export class CaptureIframeImageComponent implements OnInit {
+  @ViewChild('screen1', { static: true }) screen1: any;
+  @ViewChild('screen2', { static: true }) screen2: any;
 
-  constructor() { }
+  public capturedImage: string | undefined = undefined;
 
-  ngOnInit(): void {
+  constructor(private captureService: NgxCaptureService) {}
+
+  ngOnInit(): void {}
+
+  getBase64Image() {
+    if (this.capturedImage) {
+      return this.capturedImage;
+    }
+    return 'assets/no-image.png';
+  }
+  captureOtherDomain() {
+    this.capture(this.screen1);
   }
 
+  captureSameDomain() {
+    this.capture(this.screen2);
+  }
+
+  capture(element: ElementRef) {
+    this.captureService
+      .getImage(element.nativeElement, true)
+      .pipe(
+        tap((img) => {
+          console.log('Iframe captured');
+          this.capturedImage = img;
+        })
+      )
+      .subscribe();
+  }
 }
